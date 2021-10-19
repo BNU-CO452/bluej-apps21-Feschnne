c8187e6e240d0097341be35675ec397624c7847e
Feschnne <fescholne@gmail.com>
2021-10-19 02:43:42 +0200

    Stage2

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Felipe Schol
+ * @version 17.10.2021
  */
 public class Module
 {
@@ -15,21 +15,49 @@ public class Module
     /**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String code, String title)
     {
         // initialise instance variables
-        
+        this.code = code;
+        this.title = title;
+        credit = 0;
     }
 
     /**
      * An example of a method - replace this comment with your own
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  
+     * @return code 
      */
-    public int sampleMethod(int y)
+    public String getCode()
     {
-        // put your code here
-        return x + y;
+        return this.code;
+    }
+    
+    public String getTitle()
+    {
+        return this.title;
+    }
+    
+    public int getCredit()
+    {
+        return this.credit;
+    }
+    
+    public void setCredit(int credit)
+    {
+        this.credit = credit;
+    }
+    
+    /**
+     * Print the details of the course, the list
+     * of students enrolled and the module
+     */
+    public void print()
+    {
+        //printHeading();
+        
+        System.out.println(" Module Code: " + code + ": " + title);
+        System.out.println(" Credit: "+ credit);
     }
 }
