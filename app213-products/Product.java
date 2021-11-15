ace033e92adfb6c39532d5f1065ca40ccb18f7c9
Feschnne <fescholne@gmail.com>
2021-11-15 20:30:31 +0100

    TESTING 1&2

--- app213-products/Product.java
+++ app213-products/Product.java
@@ -1,8 +1,11 @@
-/**
+ /**
  * Model some details of a product sold by a company.
  * 
  * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * 
+ * modified by Felipe Schol Negrin
+ * 
+ * @version 15/11/2021
  */
 public class Product
 {
--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -5,7 +5,10 @@
  * the StockManager class is completed.
  * 
  * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * 
+ * modified by Felipe Schol Negrin 
+ * 
+ * @version 15/11/2021
  */
 public class StockDemo
 {
@@ -15,17 +18,27 @@ public class StockDemo
     /**
      * Create a StockManager and populate it with at least
      * 10 sample products.
+     * 
+     * 
      */
-    public StockDemo(StockList stock)
+    public StockDemo()
     {
-        this.stock = stock;
+        this.stock = new StockList();
         
         // Add at least 10 products, they must be unique to you
         // Make sure the ids are sequential numbers
         
         stock.add(new Product(101, "Samsung Galaxy S20"));
         stock.add(new Product(102, "Apple iPhone 12"));
-        stock.add(new Product(103, "Google Pixel 4A"));
+        stock.add(new Product(103, "Google Pixel 4A"));        
+        stock.add(new Product(104, "Iphone 13 Pro Max"));
+        stock.add(new Product(105, "Play Station 5"));
+        stock.add(new Product(106, "Play Station 4"));
+        stock.add(new Product(107, "Apple MacBook 2021"));
+        stock.add(new Product(108, "Apple AirPods 2nd Gen"));
+        stock.add(new Product(109, "Apple AirPods Pro"));
+        stock.add(new Product(110, "Apple Watch Series 6"));
+        runDemo();
     }
     
     /**
@@ -50,9 +63,29 @@ public class StockDemo
     
     private void buyProducts()
     {
+        stock.buyProduct(101, 500);
+        stock.buyProduct(102, 200);
+        stock.buyProduct(103, 100);
+        stock.buyProduct(104, 800);
+        stock.buyProduct(105, 250);
+        stock.buyProduct(106, 450);
+        stock.buyProduct(107, 600);
+        stock.buyProduct(108, 200);
+        stock.buyProduct(109, 900);
+        stock.buyProduct(110, 150);
     }
 
     private void sellProducts()
     {
+        stock.sellProduct(101, 300);
+        stock.sellProduct(102, 125);
+        stock.sellProduct(103, 71);
+        stock.sellProduct(104, 486);
+        stock.sellProduct(105, 121);
+        stock.sellProduct(106, 371);
+        stock.sellProduct(107, 456);
+        stock.sellProduct(108, 105);
+        stock.sellProduct(109, 717);
+        stock.sellProduct(110, 56);
     }    
 }
